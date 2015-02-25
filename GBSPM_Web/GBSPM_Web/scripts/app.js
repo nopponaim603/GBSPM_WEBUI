d29fe817bad88d455c9a7ead92df6bb318868940
noopydoopy <gtx101.love@gmail.com>
2015-02-26 01:54:18 +0700

    Combine agular with d3 prototype

--- GBSPM_Web/GBSPM_Web/scripts/app.js
+++ GBSPM_Web/GBSPM_Web/scripts/app.js
@@ -22,7 +22,7 @@ angular.module('myApp', []).
        return {
            restrict: 'E',
            replace: true,
-           template: '<div id="chart"></div>',
+           template: '',
            link: function (scope, element, attrs) {
                var data = attrs.data.split(','),
                chart = d3.select('#chart')
@@ -36,3 +36,90 @@ angular.module('myApp', []).
            }
        };
    });
+
+
+angular.module('docsSimpleDirective2', [])
+.controller('Controller', function ($scope, $http) {
+    $scope.text = "Hi My name is Tan.";
+    $scope.Address = "CNX ";
+    $scope.cities;
+    $scope.customer = {
+        name: 'Naomi',
+        address: '1600 Amphitheatre'
+    };
+    $http.get('Json/graph.json').success(function (data) {
+        // you can do some processing here
+        $scope.graph = data;
+    }).
+    error(function (data, status, headers, config) {
+        console.log("failed");
+    });
+})
+.directive('force', function () {
+    return {
+        template: 'Name: {{customer.name}} Address: {{customer.address}}',
+        link: function (scope, element, attrs) {
+            var width = 960,
+            height = 500;
+
+            var force = d3.layout.force()
+                .size([width, height])
+                .charge(-400)
+                .linkDistance(40)
+                .on("tick", tick);
+
+            var drag = force.drag()
+                .on("dragstart", dragstart);
+
+            var svg = d3.select("#chart2").append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+            var link = svg.selectAll(".link"),
+                node = svg.selectAll(".node");
+
+            d3.json("Json/graph.json", function (error, graph) {
+                force
+                    .nodes(graph.nodes)
+                    .links(graph.links)
+                    .start();
+
+                link = link.data(graph.links)
+                  .enter().append("line")
+                    .attr("class", "link");
+
+                node = node.data(graph.nodes)
+                  .enter().append("circle")
+                    .attr("class", "node")
+                    .attr("r", 20)
+                    .on("dblclick", dblclick)
+                    .call(drag);
+
+                node.on("contextmenu", function (data, index) {
+                    alert(data.color);
+                    d3.event.preventDefault();
+                });
+            });
+
+            function tick() {
+                link.attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
+
+                node.attr("cx", function (d) { return d.x; })
+                    .attr("cy", function (d) { return d.y; })
+                    .attr("fill", function (d) { return d.color; });
+            }
+
+            function dblclick(d) {
+                d3.select(this).classed("fixed", d.fixed = false);
+            }
+
+            function dragstart(d) {
+                d3.select(this).classed("fixed", d.fixed = true);
+            }
+    }
+    };
+});
+
