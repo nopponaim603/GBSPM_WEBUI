63d73d1519f891eaf997494a806c419ed76fd629
noopydoopy <gtx101.love@gmail.com>
2015-02-25 23:31:47 +0700

    First version of project prototype

---
+++ GBSPM_Web/GBSPM_Web/scripts/app.js
@@ -0,0 +1,38 @@
+﻿angular.module('Tan', [])
+.controller('MyFirstCtrl', function ($scope, $http) {
+    $scope.text = "Hi My name is Tan.";
+    $scope.Address = "CNX ";
+    $scope.cities;
+    $http.get('Json/testdata.json').success(function (data) {
+        // you can do some processing here
+        $scope.cities = data;
+    }).
+    error(function (data, status, headers, config) {
+        console.log("failed");
+    });
+});
+
+angular.module('Graph', [])
+.controller('MyGraph', function ($scope, $http) {
+    $scope.title = "This is example of graph using d3.js"
+});
+
+angular.module('myApp', []).
+   directive('bars', function ($parse) {
+       return {
+           restrict: 'E',
+           replace: true,
+           template: '<div id="chart"></div>',
+           link: function (scope, element, attrs) {
+               var data = attrs.data.split(','),
+               chart = d3.select('#chart')
+                 .append("div").attr("class", "chart")
+                 .selectAll('div')
+                 .data(data).enter()
+                 .append("div")
+                 .transition().ease("elastic")
+                 .style("width", function (d) { return d + "%"; })
+                 .text(function (d) { return d + "%"; });
+           }
+       };
+   });
