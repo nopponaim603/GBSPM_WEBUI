f1f68353e0b614c24dede11c3b7aa57b9ba7d91b
noopydoopy <gtx101.love@gmail.com>
2015-03-13 00:59:10 +0700

    Add GBSPM_WebApi project to the solution and managing about webapi can be running

---
+++ GBSPM_Web/GBSPM_WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace GBSPM_WebAPI
+{
+    public static class WebApiConfig
+    {
+        public const string DEFAULT_ROUTE_NAME = "DefaultApi";
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: DEFAULT_ROUTE_NAME,
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ GBSPM_Web/GBSPM_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GBSPM_WebAPI.MvcApplication" Language="C#" %>
