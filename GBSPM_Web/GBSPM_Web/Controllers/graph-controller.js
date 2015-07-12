24864c4150fc50b9b419b66b2dd62b70e17b639d
Noppon wongta <noppon_aim@hotmail.com>
2015-07-12 21:26:59 +0700

    - Add generate graph functional - Add event graph functional

--- GBSPM_Web/GBSPM_Web/Controllers/graph-controller.js
+++ GBSPM_Web/GBSPM_Web/Controllers/graph-controller.js
@@ -1,4 +1,5 @@
-﻿app.controller('graphController', function ($scope, $http, $modal, ServiceFactory) {
+﻿app.controller('graphController', function ($scope, $http, $modal, ServiceFactory, workItemsGraph) {
+
     $scope.positions;
     $scope.title = "Display Graph";
     $scope.subTitle = "All about graph is here.";
@@ -18,13 +19,214 @@
         });
     }
 
+
+
     function InjectGraphData(projectId) {
-        alert(projectId);
-        // TODO : change the graph to display by each project
+        
+        var modalcontrol = $modal;
+
+        ServiceFactory.GetGraphDataByProject(projectId).then(function (workItemGroups) {
+            // TODO : change the graph to display by each project
+            workItemsGraph( genGraph(workItemGroups) , $scope).then(function (data) {
+                //alert(data);
+            });
+        });
     }
+
+    
+
 
     $scope.OnSelectedProjectChanged = function (projectId) {
         InjectGraphData(projectId);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
+
+    $scope.OnSelectedNodeInfo = function () {
+
+        console.log(this.data('nodeType'));
+
+        if (this.data('nodeType').indexOf('Start') != 0 && this.data('nodeType').indexOf('End') != 0) {
+
+            alert("Info Mode \n" + this.id() + " : Node Type = " + this.data('nodeType') + " : Node ID = " + this.data('workId'));
+
+            // TODO : Call Modal
+            /*
+            var modalInstance = modalcontrol.open({
+                templateUrl: '/Views/modal-form/position-form.html',
+                controller: 'modalController',
+                resolve: {
+                    item: function () {
+                        return null;
+                    },
+                    comtrolName: function () {
+                        return "EditPosition";
+                    },
+                    additionalItems: function () {
+                        return null;
+                    }
+                }
+            });
+            */
+        }
+    }
+
+    $scope.OnSelectedNodeAdd = function () {
+        console.log(this.data('nodeType'));
+
+        if (this.data('nodeType').indexOf('Start') != 0 && this.data('nodeType').indexOf('End') != 0 && this.data('nodeType') != "WorkItem" ) {
+
+            alert("Info Mode \n" + this.id() + " : Node Type = " + this.data('nodeType') + " : Node ID = " + this.data('workId'));
+
+            // TODO : Call Modal
+            /*
+            var modalInstance = modalcontrol.open({
+                templateUrl: '/Views/modal-form/position-form.html',
+                controller: 'modalController',
+                resolve: {
+                    item: function () {
+                        return null;
+                    },
+                    comtrolName: function () {
+                        return "EditPosition";
+                    },
+                    additionalItems: function () {
+                        return null;
+                    }
+                }
+            });
+            */
+        }
+    }
+
+    $scope.OnSelectedNodeEdit = function () {
+        console.log(this.data('nodeType'));
+    }
+
+    $scope.OnSelectedNodeDelete = function () {
+        console.log(this.data('nodeType'));
+    }
+
+    function genGraph( data ) {
+    
+        var nodes  = [];
+  
+        var edges = [];
+
+        // TODO : Add Node Start
+        nodes.push({
+            id: 'start',
+            nodeType: 'Start',
+            faveShape: 'ellipse',
+            faveColor: '#056B22',
+            name: 'start',
+            isCritical: true
+        });
+
+
+        for (var index = 0; index < data.length; index++) {
+
+            var workItemGroup = data[index];
+
+            // TODO : Add Node WorkItemGroups
+            console.log(data[index].WorkItemGroupId);
+            nodes.push({
+                id: 'wg' + data[index].WorkItemGroupId.toString(),
+                nodeType: 'WorkItemGroup',
+                faveShape: 'ellipse',
+                faveColor: '#3581B7',
+                name: 'WorkItemGroupID : ' + data[index].WorkItemGroupId,
+                workId: data[index].WorkItemGroupId,
+                isCritical: true
+            });
+
+            // TODO : Link start to work
+            if (index == 0) {
+                edges.push({
+                    source: 'start',
+                    target: 'wg' + data[index].WorkItemGroupId.toString(),
+                    faveColor: '#C62828'
+                });
+            }
+
+            
+            for (var i = 0 ; i < workItemGroup.WorkItems.length; i++) {
+                console.log(data[index].WorkItems[i].WorkItemId);
+
+                // TODO : Add Node WorkItems
+                nodes.push({
+                    id: 'wi' + data[index].WorkItems[i].WorkItemId.toString(),
+                    nodeType: 'WorkItem',
+                    faveShape: 'roundrectangle',
+                    faveColor: '#B2B2B2',
+                    name: 'WorkItemID : ' + data[index].WorkItems[i].WorkItemId,
+                    workId: data[index].WorkItems[i].WorkItemId,
+                    isCritical: true
+                });
+
+                // TODO : Link WorkGroupItems to WorkItems
+                edges.push({
+                    source: 'wg' + data[index].WorkItemGroupId.toString(),
+                    target: 'wi' + data[index].WorkItems[i].WorkItemId.toString(),
+                    faveColor: function () {
+                        if (true) {
+                            return '#C62828';
+                        } else {
+                            return '#333333';
+                        }
+                    }
+                });
+
+
+                if (index + 1 < data.length) {
+                    // TODO : Link WorkItem to Next WorkGroupItems
+                    edges.push({
+                        source: 'wi' + data[index].WorkItems[i].WorkItemId.toString(),
+                        target: 'wg' + data[index + 1].WorkItemGroupId.toString(),
+                        faveColor: function () {
+                            if (true) {
+                                return '#C62828';
+                            } else {
+                                return '#333333';
+                            }
+                        }
+                    });
+
+                } else {
+                    // TODO : Link WorkItem to End Node.
+                    edges.push({
+                        source: 'wi' + data[index].WorkItems[i].WorkItemId.toString(),
+                        target: 'end',
+                        faveColor: function () {
+                            if (true) {
+                                return '#C62828';
+                            } else {
+                                return '#333333';
+                            }
+                        }
+                    });
+                }
+            }
+
+
+
+
+        }
+
+        // TODO : Add Node End
+        nodes.push({
+            id: 'end',
+            nodeType: 'End',
+            faveShape: 'ellipse',
+            faveColor: '#056B22',
+            name: 'end',
+            isCritical: true
+        });
+
+
+        var result = { "nodes": nodes, "edges": edges };
+       return result;
     }
 
     InjectData();
---
+++ GBSPM_Web/GBSPM_Web/scripts/GraphServices.js
@@ -0,0 +1,124 @@
+﻿app.service('workItemsGraph', ['$q', function ($q) {
+    var cy;
+    var workItemsGraph = function (myGraph, $scope) {
+        var workItem = myGraph.nodes;
+        var edges = myGraph.edges;
+        var deferred = $q.defer();
+
+        // put people model in cy.js
+        var eles = [];
+
+        //////////////////////////////////////////////
+        /////////////// push Node workItem ///////////
+        for (var i = 0; i < workItem.length; i++) {
+            eles.push({
+                group: 'nodes',
+                data: {
+                    id: workItem[i].id,
+                    name: workItem[i].name,
+                    faveShape: workItem[i].faveShape,
+                    faveColor: workItem[i].faveColor,
+                    nodeType: workItem[i].nodeType,
+                    workId:   workItem[i].workId,
+                    isCritical: true,
+                    "localization": "localization"
+                }
+            });
+        }
+
+        ////////////////////////////////////////////////////////
+        /////////////// push edges  to link workItem ///////////
+        for (var i = 0; i < edges.length; i++) {
+            console.log(edges[i].source + " : " + edges[i].target)
+            eles.push({
+                group: 'edges',
+                data: {
+                    source: edges[i].source,
+                    target: edges[i].target,
+                    faveColor: edges[i].faveColor,
+                    "idgroup": edges[i].source
+                }
+            });
+        }
+        /////////////////////////////////////////////////////
+
+        $(function () { // on dom ready
+
+            cy = cytoscape({
+                container: $('#cy')[0],
+
+                style: //cerebral_style
+                cytoscape.stylesheet()
+                  .selector('node')
+                    .css({
+                        'content': 'data(name)',
+                        'shape': 'data(faveShape)',
+                        'text-outline-color': 'data(faveColor)',
+                        'background-color': 'data(faveColor)',
+                        'text-valign': 'center',
+                        'text-outline-width': 2,
+                    })
+                  .selector('edge')
+                    .css({
+                        'target-arrow-shape': 'triangle',
+                        'line-color': 'data(faveColor)',
+                        'source-arrow-color': 'data(faveColor)',
+                        'target-arrow-color': 'data(faveColor)'
+                    })
+              ,
+
+                layout: {
+                    name: 'breadthfirst',
+                    directed: true,
+                    padding: 10,
+                    rotation: 90
+                },
+
+                elements: eles,
+                zoomingEnabled: false,
+                fit: true,
+                ready: function () {
+                    deferred.resolve(this);
+
+                }
+            });
+
+            
+            cy.cxtmenu({
+
+                selector: 'node',
+
+                commands: [
+                    {
+                        content: '<span class="fa fa-info fa-3x"></span>',
+                        select: $scope.OnSelectedNodeInfo
+                            
+                    },
+
+                    {
+                        content: '<span class="fa fa-plus fa-3x"></span>',
+                        select: $scope.OnSelectedNodeAdd
+                    },
+
+                    {
+                        content: '<span class="fa fa-pencil fa-3x"></span>',
+                        select: $scope.OnSelectedNodeEdit
+                    },
+
+                    {
+                        content: '<span class="fa fa-warning fa-3x"></span>',
+                        select: $scope.OnSelectedNodeDelete
+                    }
+                ]
+
+            });
+
+            
+        }); // on dom ready
+
+        return deferred.promise;
+    };
+
+
+    return workItemsGraph;
+}]);
